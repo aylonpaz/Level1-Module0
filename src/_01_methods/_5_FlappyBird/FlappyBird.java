f158e4ddcccd675a638cdffdaa399af041cd3122
league <league@192.168.1.200>
2026-06-05 18:27:26 -0700

    asdfasfsafsafas

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -1,6 +1,9 @@
 package _01_methods._5_FlappyBird;
 
+import javax.swing.JOptionPane;
+
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class FlappyBird extends PApplet {
     static final int WIDTH = 800;
@@ -8,17 +11,19 @@ public class FlappyBird extends PApplet {
     int birdX = 150;
     int birdY = 300;
     int birdVelocity = -10;
-    int fallSpeed = 1;
     int framesDrawn = 0;
+    int gravity = 1;
+    PImage topPipe;
     @Override
     public void settings() {
         size(WIDTH, HEIGHT);
-        
+       
     }
 
     @Override
     public void setup() {
-       
+        frameRate(60);
+        topPipe = loadImage("src/_01_methods/_5_FlappyBird/topPipe.png");
     }
 
     @Override
@@ -27,11 +32,19 @@ public class FlappyBird extends PApplet {
      fill(200, 40, 10);
      stroke(0,0,0);
      ellipse(birdX,birdY,30,30);
-     if(framesDrawn %2 == 0) {
-     birdVelocity -= fallSpeed;
-     birdY -= birdVelocity;
+     image(topPipe,350, 0);
+    // if(framesDrawn %2 == 0) {
+     birdY += birdVelocity;
+     birdVelocity += gravity;
+     if(mousePressed || keyPressed) {
+    	 birdVelocity = -10;
      }
-     framesDrawn +=1;
+     if(birdY >= height + 50 || birdY <= -50) {
+    	 JOptionPane.showMessageDialog(null, "You lost!");
+    	 exit();
+     }
+    // }
+    // framesDrawn +=1;
     }
 
     static public void main(String[] args) {
